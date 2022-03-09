acb030e3ea8ce46669780412e001a57d3e328078
Will Gundy <willgunderson503@gmail.com>
2022-03-09 14:28:01 -0800

    Finished event listeners for mid and bottom dropdowns

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ let bottomCount = 0;
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-    const headDropdownValue = 'assets/' + headDropdown.value + '-head.png';
-    console.log(headDropdownValue);
+    const value = 'assets/' + headDropdown.value + '-head.png';
+    
     // increment the head change count state
     headCount++;
     // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-    headEl.style.backgroundImage = `url("${headDropdownValue}")`;
+    headEl.style.backgroundImage = `url("${value}")`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
@@ -32,23 +32,29 @@ headDropdown.addEventListener('change', () => {
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-
+    const value = 'assets/' + middleDropdown.value + '-middle.png';
     // increment the middle change count state
+    bodyCount++;
     
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
+    middleEl.style.backgroundImage = `url("${value}")`;
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-
+    const value = 'assets/' + bottomDropdown.value + '-pants.png';
     // increment the bottom change count state
+    bottomCount++;
     
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
+    bottomEl.style.backgroundImage = `url("${value}")`;
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
